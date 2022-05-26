fddf7181c354c70dde4b0f375edfd22d5a07210f
isabella-66 <amasprog@gmail.com>
2022-05-26 02:21:25 -0300

    Changing the application title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppComponentComponent } from './app-component/app-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppComponentComponent
   ],
   imports: [
     BrowserModule,
