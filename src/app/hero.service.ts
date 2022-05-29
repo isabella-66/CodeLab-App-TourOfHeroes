9b37f013773103dbf04d56ed9c6b3e4e2841678c
isabella-66 <amasprog@gmail.com>
2022-05-29 19:46:07 -0300

    Adding HttpClient - GET

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -7,26 +8,31 @@ import { MessageService } from './message.service';
 /* to get data from the server */
 import { Observable, of } from 'rxjs';
 
+
 @Injectable({
   //provider metadata, can create or deliver a service
   providedIn: 'root',
 })
 export class HeroService {
 
-  constructor(private messageService : MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
-  /* asynchronous approach for requests from the server */
+  /* URL to web api */
+  private heroesUrl = 'api/heroes';
+  
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /* get heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    /* message sent when it fetches heroes */
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(hero);
-    //throw new Error('Method not implemented.');
+    /* throw new Error('Method not implemented.'); */
   }
 }
