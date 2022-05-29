bbc3718dc4e57f75c10b3cac753f0b9953243dc0
isabella-66 <amasprog@gmail.com>
2022-05-29 19:21:58 -0300

    Importing 'HttpClientModule' and 'InMemoryDataService' to simulate a data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientInMemoryWebApiModule, InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService } from "./in-memory-data.service";
+
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-@NgModule({
+@NgModule ({
   declarations: [
     AppComponent,
     HeroesComponent,
@@ -21,9 +26,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+
+    /* intercepts HTTP requests and returns simulated server responses, until  real server is ready to receive requests */
+    HttpClientInMemoryWebApiModule.forRoot(
+      /* primes the in-memory database */
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+  
+  createDb() {
+    const heroes = [
+      { id: 12, name: 'Willem Arondeus'},
+      { id: 13, name: 'Theresa Kachindamoto' },
+      { id: 14, name: 'Aitzaz Hasan' },
+      { id: 15, name: 'Charles Lightoller' },
+      { id: 16, name: 'Heley de Abreu' },
+      { id: 17, name: 'Mahatma Ghandi' },
+      { id: 18, name: 'Nelson Mandela' },
+      { id: 19, name: 'Martin Luther King Jr' },
+      { id: 20, name: 'Malala Yousafzai' }
+    ];
+    return heroes;
+  }
+
+  /* ensure that a hero always has an id */
+  genId(heroes: Hero[]): number {
+    /* if heroes array is empty */
+    return heroes.length > 0 ?
+    /* no, the method below returns the highest hero (id + 1) */
+    Math.max(...heroes.map(hero => hero.id)) + 1 
+    /* yes, the method below returns the initial number (11)*/
+    : 11;
+  }
+
+  constructor() { }
+}
