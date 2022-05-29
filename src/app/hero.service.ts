33d62c482004620c6c4a92d8d29f37001470c579
isabella-66 <amasprog@gmail.com>
2022-05-28 22:58:40 -0300

    Programming to display the message when the heroes are fetched and a button to clear the messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 /* to get data from the server */
 import { Observable, of } from 'rxjs';
@@ -12,11 +13,13 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService : MessageService) { }
 
   /* asynchronous approach for requests from the server */
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    /* message sent when it fetches heroes */
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,7 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class MessageService {
+  messages: string[] = [];
+
+  /* add a message to the cashe */
+  add(message : string) {
+    this.messages.push(message);
+  }
+
+  /* clear the cache */
+  clear() {
+    this.messages = [];
+  }
 
   constructor() { }
 }
