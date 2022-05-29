5435e9c213f0e8eab4a32dfe002ee4dfa99b0f09
isabella-66 <amasprog@gmail.com>
2022-05-29 15:14:05 -0300

    Programming navigation through hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,29 +11,21 @@ import { MessageService } from '../message.service';
 })
 
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
 
   /* exposing heroes array */
   heroes: Hero[] = [];
 
   /* sets the heroService parameter to the singleton instance of HeroService */
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   /* for initialization logic */
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero) : void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   /* retrieve the heroes from the service */
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-
-  
+  }  
 }
