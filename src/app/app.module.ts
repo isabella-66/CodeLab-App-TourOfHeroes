802c627953573ec44330800387897ffde337c0eb
isabella-66 <amasprog@gmail.com>
2022-05-29 22:40:07 -0300

    Searching a hero in an economic way - SEARCH

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule ({
   declarations: [
@@ -22,6 +23,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -62,12 +62,29 @@ export class HeroService {
     );
   }
 
-  /** DELETE: delete the hero from the server */
+  /* DELETE: delete the hero */
   deleteHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /* SEARCH: get heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    /* if not search term, return empty hero array */
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+            tap(
+              x => x.length ?
+              this.log(`found heroes matching "${term}"`) :
+              this.log(`no heroes matching "${term}"`)
+            ),
+            catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
 
